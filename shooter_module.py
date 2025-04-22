1e3371b6ba46fec41029e814fab0507167c5ff16
n00bNUddles <n00bNUddles@gmail.com>
2025-04-21 22:25:25 -0700

    Change shooter controls to use D-Pad left/right instead of A/B buttons

--- shooter_module.py
+++ shooter_module.py
@@ -3,22 +3,22 @@ from phoenix6.hardware import TalonFX
 from phoenix6.controls import DutyCycleOut
 
 class shooter:
-    def __init__(self, motor, controller, speed=0.5):
+    def __init__(self, motor, controller, speed=0.25):
         self.motor = motor
         self.controller = controller
         self.SPEED = speed
 
     def update_shooter(self):
-        """Update shooter motor based on button inputs."""
-        # Get button states
-        a_pressed = self.controller.getAButton()
-        b_pressed = self.controller.getBButton()
+        """Update shooter motor based on D-Pad inputs."""
+        # Get D-Pad states
+        dpad_left = self.controller.getPOV() == 270  # D-Pad left is 270 degrees
+        dpad_right = self.controller.getPOV() == 90  # D-Pad right is 90 degrees
 
-        # Determine motor speed based on button inputs
+        # Determine motor speed based on D-Pad inputs
         motor_speed = 0.0
-        if a_pressed:
+        if dpad_right:
             motor_speed = self.SPEED
-        elif b_pressed:
+        elif dpad_left:
             motor_speed = -self.SPEED
 
         # Set motor to calculated speed
